8346132ec6f5d24a8b7dada412dd17c76a45a140
AkiraQZ <vboyrkin@gmail.com>
2024-02-11 03:08:24 +0800

    fix: move .env from src to main directory

--- cloth-shop/app/server/src/index.js
+++ cloth-shop/app/server/src/index.js
@@ -1,13 +1,27 @@
 require('dotenv').config()
 const express = require('express');
+const sequelize = require("./db.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+async function start() {
+    try {
+        await sequelize.authenticate()
+        .then(() => {
+            console.log('DB authorized');
+        })
+        await sequelize.sync()
+        .then (() => {
+            console.log('DB sync');
+        })
+        app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
 // gentlemens, start your engines!!!
 // try/catch just to make sure the server starts without errors
-try {
-    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-} catch (error) {
-    console.error(error.message);
-}
+
+start();
