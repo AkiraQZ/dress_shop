a3082b6ce881e18aecfeb5fad2e013a7a1f780b1
AkiraQZ <vboyrkin@gmail.com>
2024-02-24 12:43:00 +0800

    feat: auth middleware, basket controller (not implemented)

--- cloth-shop/app/server/src/controllers/user_controller.js
+++ cloth-shop/app/server/src/controllers/user_controller.js
@@ -1,27 +1,56 @@
-const apiError = require('../error/api_error.js')
+const apiError = require('../error/api_error.js');
+const {user, basket} = require('../models/models.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+function genJwt(id, email, role) {
+  return jwt.sign({id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'});
+}
 class userController {
-    async registration(req, res) {
+  async registration(req, res, next) {
+    const {email, password, role} = req.body
+    if (!email || !password) {
+        return next(apiError.badRequest('Некорректный email или password'))
+    }
+    const candidate = await user.findOne({where: {email}})
+    if (candidate) {
+        return next(apiError.badRequest('Пользователь с таким email уже существует'))
+    }
+    const hashPassword = await bcrypt.hash(password, 5)
+    const userItem = await user.create({email, role, password: hashPassword})
+    const basketItem = await basket.create({userId: user.id})
+    const token = genJwt(userItem.id, userItem.email, userItem.role)
+    return res.json({token})
+}
 
-    };
-
-    async login(req, res) {
-
-    };
+    async login(req, res, next) {
+      const {email, password} = req.body
+      const userItem = await user.findOne({where: {email}})
+      if (!userItem) {
+          return next(apiError.internal('User not found'))
+      }
+      let comparePassword = bcrypt.compareSync(password, userItem.password)
+      if (!comparePassword) {
+          return next(apiError.internal('Incorrect password'))
+      }
+      const token = genJwt(userItem.id, userItem.email, userItem.role)
+      return res.json({token})
+    }
 
     async check(req, res, next) {
-        const {id} = req.query;
-        if(!id){
-            return next(apiError.badRequest('ID not found'));
-        };
-        res.json(id);
-    };
+      const token = genJwt(req.user.id, req.user.email, req.user.role)
+      return res.json({token})
+  }
 
     async  delete(req, res, next) {
         try {
-          const id = parseInt(req.params.id, 10);
+          const { id } = req.body;
 
-          const success = await user.delete(id);
+          const success = await user.destroy({
+            where: {
+              "id": id
+            },
+          });
 
           if (success) {
             res.status(204).end();
--- cloth-shop/app/server/src/routes/user_router.js
+++ cloth-shop/app/server/src/routes/user_router.js
@@ -1,10 +1,11 @@
 const Router = require ('express');
 const router = new Router();
 const userController = require ('../controllers/user_controller.js');
+const auth_midleware = require ('../middleware/auth_midleware.js');
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.check)
+router.get('/auth', auth_midleware, userController.check)
 router.delete('/', userController.delete)
 
 module.exports = router;
